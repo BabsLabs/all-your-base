c3998d056184a70b31fd87a588ba533768182686
Brian Bower <brian@brianbower.com>
2020-01-30 19:13:57 -0700

    Refactor the forecast data that is returned as json to be use javascript objects instead of iterating over the keys returned and deleting them.

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -1,5 +1,5 @@
 const dotenv = require('dotenv').config();
-// let googleService = require("../../../services/google_service");
+const Forecast = require('../../../pojos/forecast');
 
 var express = require('express');
 var router = express.Router();
@@ -10,8 +10,9 @@ const database = require('knex')(configuration);
 const fetch = require('node-fetch');
 
 router.get('/', (request, response) => {
+  // CHECK THAT AN API KEY AND LOCATION WERE SENT WITH THE REQUEST
   if ((request.body.api_key) && (request.query.location)){
-
+    // QUERY THE DATABASE TO CHECK FOR A USER WITH THE PASSED IN API KEY
     database('users').where('api_key', request.body.api_key).first()
       .then((user) => {
 
@@ -40,66 +41,8 @@ router.get('/', (request, response) => {
                return response.json();
             })
             .then((json) => {
-
-              //
-
-              // CURRENT WEATHER INFO
-              let currentWeather = json.currently;
-              // CLEAN UP THE CURRENT WEATHER INFO
-              delete currentWeather.time;
-              delete currentWeather.nearestStormDistance;
-              delete currentWeather.nearestStormBearing;
-              delete currentWeather.apparentTemperature;
-              delete currentWeather.dewPoint;
-              delete currentWeather.uvIndex;
-              delete currentWeather.ozone;
-
-              // HOURLY WEATHER INFO
-              let hourlyWeather = json.hourly;
-              let hourlyWeatherSumamry = json.hourly.summary;
-              let hourlyWeatherIcon = json.hourly.icon;
-              let eightHourForecast = hourlyWeather.data.slice(0, 8);
-              // CLEAN UP THE EIGHT HOUR FORECAST INFO
-              eightHourForecast.forEach((day, index) => {
-                delete day.precipType;
-                delete day.precipAccumulation;
-                delete day.apparentTemperature;
-                delete day.dewPoint;
-                delete day.uvIndex;
-                delete day.ozone;
-                return eightHourForecast;
-              });
-
-              // DAILY WEATHER INFO
-              let dailyWeather = json.daily;
-              let dailyWeatherSumamry = json.daily.summary;
-              let dailyWeatherIcon = json.daily.icon;
-              let sevenDayForecast = dailyWeather.data.slice(0, 7);
-              // CLEAN UP THE SEVEN DAY FORECAST WEATHER INFO
-              sevenDayForecast.forEach((day, index) => {
-                delete day.moonPhase;
-                delete day.precipAccumulation;
-                delete day.temperatureHighTime;
-                delete day.temperatureLowTime;
-                delete day.apparentTemperatureHigh;
-                delete day.apparentTemperatureHighTime;
-                delete day.apparentTemperatureLow;
-                delete day.apparentTemperatureLowTime;
-                delete day.dewPoint;
-                delete day.windGustTime;
-                delete day.windBearing;
-                delete day.uvIndex;
-                delete day.uvIndexTime;
-                delete day.ozone;
-                delete day.temperatureMinTime;
-                delete day.temperatureMaxTime;
-                delete day.apparentTemperatureMin;
-                delete day.apparentTemperatureMinTime;
-                delete day.apparentTemperatureMax;
-                delete day.apparentTemperatureMaxTime;
-                return sevenDayForecast;
-              });
-              response.status(200).json({currently: currentWeather, hourly: {summary: hourlyWeatherSumamry, icon: hourlyWeatherIcon, data: eightHourForecast}, daily: {summary: dailyWeatherSumamry, icon: dailyWeatherIcon, data: sevenDayForecast}});
+              // // RETURN CURRENT WEATHER INFO
+              response.status(200).json(new Forecast(location, json));
             });
           // END OF DARSKY API FETCHING
         });
