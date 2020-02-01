6daf442cb24061db2ba255ea81ebcb0a60bc5f13
Brian Bower <brian@brianbower.com>
2020-02-01 14:42:09 -0700

    Remove the explicit get from the fetch request as it is implied and not needed

--- services/google_service.js
+++ services/google_service.js
@@ -4,7 +4,7 @@ async function getLatAndLongFromGoogle(location) {
   const googleApiKey = process.env.GOOGLE_API_KEY;
   const googleApiUrl =`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${googleApiKey}`;
 
-  let response = await fetch(googleApiUrl, { method: 'GET'});
+  let response = await fetch(googleApiUrl);
   let googleGeocodeResponse = await response.json();
   const latAndLng = googleGeocodeResponse.results[0].geometry.location;
   const joinedLatAndLng = `${latAndLng.lat},${latAndLng.lng}`;
