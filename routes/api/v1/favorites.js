f1e9cae3d8c7429e965b476cf3c30f2f8430c229
Brian Bower <brian@brianbower.com>
2020-01-31 23:12:46 -0700

    Refactor to use a FavoriteForecast object. Currently only returns the first object so the obejct needs pushed into an array and the array returned.

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -2,6 +2,10 @@ const dotenv = require('dotenv').config();
 var express = require('express');
 var router = express.Router();
 
+const getLatAndLongFromGoogle = require('../../../services/google_service');
+const Forecast = require('../../../pojos/forecast');
+const FavoriteForecast = require('../../../pojos/favorite_forecast');
+
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
@@ -72,29 +76,49 @@ router.get('/', (request, response) => {
     if (request.body.api_key) {
     database('users').where('api_key', request.body.api_key).first()
       .then((user) => {
-        if (user && request.body.location) {
-
-          let favoriteLocation = request.body.location;
+        if (user) {
 
           database('favorites').where('user_id', user.id)
-            .then(favoriteLocation => {
-              
-              let favoriteLocationArray = [];
-              favoriteLocation.forEach((location, i) => {
-                favoriteLocationArray.push(location.location);
+            .then(favoriteLocations => {
+              if (favoriteLocations.length) {
+
+              favoriteLocations.forEach((location, i) => {
+
+
+
+                const googleResults = getLatAndLongFromGoogle(location);
+                googleResults.then( (latAndLongGoogleResults) => {
+                // DARK SKY METHOD GOES HERE
+                  const darkSkyApiKey = process.env.DARKSKY_API_KEY;
+                  const darkSkyUrl = `https://api.darksky.net/forecast/${darkSkyApiKey}/${latAndLongGoogleResults}?exclude=minutely,flags&units=us`;
+                  fetch(darkSkyUrl, { method: 'GET'})
+                    .then((response) => {
+                       return response.json();
+                    })
+                    .then((json) => {
+                      // // RETURN CURRENT WEATHER INFO
+
+                      // ONLY RETURNS THE FIRST ONE FOR NOW. NEEDS TO BE PUSHED INTO THE FORECAST ARRAY AND THEN HAVE THAT RETUNED AS THE RESPONSE
+                      response.status(200).json(new FavoriteForecast(new Forecast(location, json)));
+                    }).catch(error => console.log(error));
+                  // END OF DARSKY API FETCHING
+                  });
+
+
+
+
+
+
               });
-              console.log(favoriteLocationArray);
+              // response.status(200).json({ message: 'WOWOWOW!' });
 
-
-              response.status(200).json(favoriteLocation);
+            } else {
+                response.status(200).json({ message: 'No favorites found.' });
+              }
             })
             .catch(error => {
               response.status(500).json({ error });
             });
-
-
-        } else if (!request.body.location && user ){
-          response.status(400).json({error: 'Bad Request! Are you missing a location?'});
         } else if (!user) {
           response.status(401).json({error: 'Unauthorized!'});
         }
