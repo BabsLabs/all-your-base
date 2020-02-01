398b1d5a5ef72235625741d20f907817575b213d
Brian Bower <brian@brianbower.com>
2020-01-31 21:16:14 -0700

    Add get '/' route that makes an array of the users favorite locaitons. These favorite locations will be iterated though for a forecast

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -68,4 +68,40 @@ router.delete('/', (request, response) => {
     }
 });
 
+router.get('/', (request, response) => {
+    if (request.body.api_key) {
+    database('users').where('api_key', request.body.api_key).first()
+      .then((user) => {
+        if (user && request.body.location) {
+
+          let favoriteLocation = request.body.location;
+
+          database('favorites').where('user_id', user.id)
+            .then(favoriteLocation => {
+              
+              let favoriteLocationArray = [];
+              favoriteLocation.forEach((location, i) => {
+                favoriteLocationArray.push(location.location);
+              });
+              console.log(favoriteLocationArray);
+
+
+              response.status(200).json(favoriteLocation);
+            })
+            .catch(error => {
+              response.status(500).json({ error });
+            });
+
+
+        } else if (!request.body.location && user ){
+          response.status(400).json({error: 'Bad Request! Are you missing a location?'});
+        } else if (!user) {
+          response.status(401).json({error: 'Unauthorized!'});
+        }
+      }).catch(error => console.log(error));
+    } else {
+      response.status(400).json({error: 'Bad Request! Did you send an Api Key?'});
+    }
+});
+
 module.exports = router;
